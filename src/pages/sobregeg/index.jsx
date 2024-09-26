d47cf3f48b1344364b581b06ae4e39c77b32e4f8
Maria Fernanda Vieira de Almeida <ra56756282824@cursos.insf>
2024-09-26 16:45:08 -0300

    sobre

--- src/pages/sobregeg/index.jsx
+++ src/pages/sobregeg/index.jsx
@@ -26,7 +26,7 @@ export default function SobreGeG() {
                 <p>Profissional com mais de 10 anos de experiência nas áreas de Recursos Humanos e Administrativa, com carreira desenvolvida em empresas de pequeno e grande porte.</p>
             </div>
             <div className="textlupa">
-                <img src="/assets/images/sobre/lupa.png" alt="" /> <h1>Há 10 anos movimentando o mercado de trabalho</h1>
+                <img className='imglupa' src="/assets/images/sobre/lupa.png" alt="" /> <h1 className='h1'>Há 10 anos movimentando o mercado de trabalho</h1>
             </div>
 
             <Tituloelogo  titulo = 'SERVIÇOS G&G'/>
@@ -34,15 +34,32 @@ export default function SobreGeG() {
 
                 <div className="conteudo">
                 <div className="texto">
-                    <h1>Os serviços da G&G tem como foco a otimização de tarefas das áreas de negócio. O objetivo  da G&G está em facilitar o processo de contratação 
-                    tanto para os recrutadores quanto para os candidatos das vagas. Tudo isso resulta em uma maior produtividade e economia.</h1>
+                    <p>Os serviços da <strong>G&G </strong> tem como foco a <strong>otimização de tarefas das áreas de negócio.</strong> O objetivo  da G&G está em facilitar o processo de contratação 
+                    tanto para os recrutadores quanto para os candidatos das vagas. Tudo isso resulta em uma maior <strong>produtividade e economia.</strong> </p>
                 </div>
                 
                     <img className='imagem' src="/assets/images/sobre/servico.png" alt="" />
           
                 </div>
-                
             </div>
+
+            <Tituloelogo  titulo = 'SOBRE NOSSO AUTÔNOMO'/>
+            <div className="cartaoautonomo">
+
+            <div className="cont">
+            <div className="textautonomo">
+                <h1 className='titulogis'>GISLAINE</h1>
+                <h1 className='textogislaine'>Meu nome é <strong>Gislaine</strong>, sou casada e mãe de quatro filhos além de ter uma carreira de <strong>dez anos na área de recursos humanos.</strong>  Atualmente coordeno uma equipe se seleção, trabalhando com vagas administrativa e operacionais. Basicamente <strong>a área de R&S é a área responsável pela captação, divulgação de vagas, triagem de currículos e alinhamento de perfil junto ao requisitante,</strong> realizo entrevista por competência e aplico testes e encaminho o candidato para vaga. Sou apaixonada pelo que faço o que tora meu trabalho fácil de ser administrado. Não recruto currículos e sim pessoas com vontade e motivação. O currículos nos mostra a experiência necessária pra desempenhar a função.</h1>
+
+
+            </div>
+
+                <img className='gislaine' src="/assets/images/sobre/autonomo.png" alt="" />
+
+            </div>
+            </div>
+            
+
 
 
 
--- src/pages/vagas/index.jsx
+++ src/pages/vagas/index.jsx
@@ -1,9 +1,21 @@
 
 import './index.scss'
+import Tituloelogo from '../../componentes/tituloelogo'
+import Cabecalho from '../../componentes/cabeçalho'
 
 export default function Vagas() {
     return (
         <div className='pagina-vagas'>
+            <Cabecalho/>
+            <Tituloelogo titulo = 'VAGAS'/>
+
+            <div className="botoes">
+
+            <a href="">Filtros</a>
+            <a href="">Cargos</a>
+            <a href="">Local</a>
+            </div>
+
            
         </div>
     )
