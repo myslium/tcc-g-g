fd4471040bf6e596486639e9041667b3fec768a2
Maria Fernanda Vieira de Almeida <ra56756282824@cursos.insf>
2024-09-26 17:43:07 -0300

    comecando vagas

--- src/pages/vagas/index.jsx
+++ src/pages/vagas/index.jsx
@@ -6,14 +6,36 @@ import Cabecalho from '../../componentes/cabeçalho'
 export default function Vagas() {
     return (
         <div className='pagina-vagas'>
+           
             <Cabecalho/>
-            <Tituloelogo titulo = 'VAGAS'/>
+            <div className="topo">
+                <div className="conttopo">
+                <img className='geg' src="/assets/images/cabecalho/logo.png" alt="" />
+                <h1>VAGAS</h1>
+           
+            </div>
+            </div>
 
             <div className="botoes">
 
-            <a href="">Filtros</a>
-            <a href="">Cargos</a>
-            <a href="">Local</a>
+           
+            </div>
+
+            <div className="vagas">
+
+
+                <div className="cartavaga">
+                    <div className="textovaga">
+                        <h1>Analista de Suporte em TI</h1>
+                        <p>Sobre a vaga: - Estamos procurando por uma pessoa alinhada com nossos valores para atuar como Técnico de Informática, em um contexto dinâmico de uma empresa em ascensão no mercado…</p>
+                        <a href="">Mais sobre</a>
+                    </div>
+                    <div className="image">
+                        <img className='vagaimg' src="/assets/images/vagas/TI.png" alt="" />
+                        <a className='botaovaga' href="">Candidatar-se</a>
+                    </div>
+                    
+                </div>
             </div>
 
            
