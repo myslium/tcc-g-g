69d2e2ee760dac55c7709e3de48dec66e8f9ac73
Mariana <165081625+myslium@users.noreply.github.com>
2024-09-28 17:42:23 -0300

    salvando por enquanto

--- src/pages/falecomconsultor/index.jsx
+++ src/pages/falecomconsultor/index.jsx
@@ -1,10 +1,51 @@
-
+import Cabecalho from '../../componentes/cabeçalho'
+import Tituloelogo from '../../componentes/tituloelogo'
 import './index.scss'
 
 export default function Falecomconsultor() {
     return (
         <div className='pagina-falecomconsultor'>
-           
+           <Cabecalho/>
+
+            <section className='primeira-parte'>
+                <img src="/assets/images/consultor/primeiraimg.png" alt="" />
+            </section>
+
+            <Tituloelogo 
+            titulo = 'POR QUE TRABALHAR COM A G&G?'/>
+
+
+            <section className='segunda-parte'>
+
+                <div className='primeira-div'>
+
+
+                    <div className='texto'>
+                    <p>A G&G tem trazido resultados significativos para seus clientes, reduzindo índices de turnover de novos colaboradores, tornando o processo seletivo mais justo e promovendo a <b>diversidade e inclusão de forma genuína, gerando valor para nossos clientes.</b></p>
+
+                    </div>
+                  
+                    <img className='geg-img' src="/assets/images/consultor/pessoasmesa.png" alt="Ambiente de trabalho Gente&Gestão" />
+                </div>
+
+                <div className='segunda-div'>
+
+                    <div className='filhos'>
+
+                        <img className='img-filho'  src="/assets/images/consultor/docapertodemao1.png" alt="Aperto de mão Gente&Gestão" />
+                        <h3>+de 80% dos nossos clientes estão satisfeitos</h3>
+                    </div>
+
+                    <div className='filhos'>
+
+                        <img className='img-filho'   src="/assets/images/consultor/apertodemao.png" alt="Aperto de mão Gente&Gestão" />
+
+                        <h3>+de 1.500 contratados</h3>
+                        
+                     
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
--- src/pages/inicio/index.jsx
+++ src/pages/inicio/index.jsx
@@ -91,8 +91,24 @@ export default function Inicio() {
 
            </div>
            <div className="ultima">
-            <h1 className='texto'><img src="/assets/images/cabecalho/logo.png" alt="" />Quer aumentar a produtividade da sua empresa e economizar?</h1>
-            <button>Fale com o consultor</button>
+            
+            <div className='consultor'>
+
+                <div className='frase'>
+
+                    <img src="/assets/images/cabecalho/logo.png" alt="Logo" />
+                    <h1>Quer aumentar a produtividade da sua empresa e economizar?</h1>
+                </div>
+
+                <Link to = '/falecomconsultor'>
+                <button className='botao'>Fale com o consultor</button>
+                
+                </Link>
+              
+
+            </div>
+
+            <img className='ultima-imagem'  src="/assets/images/inicio/ultima.png" alt="imagem Gente&Gestão" />
 
            </div>
            <footer className="footer">
