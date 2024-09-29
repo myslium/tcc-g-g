e861afad082a2c99032a5dc593f4d06a25c13b5a
jucastro15 <ra56421610838@acaonsfatima.org.br>
2024-09-28 22:40:58 -0300

    ta

--- src/componentes/footer/index.jsx
+++ src/componentes/footer/index.jsx
@@ -5,24 +5,28 @@ export default function Footer() {
 
     return (
         <footer className="footer">
-            
-        <div className="footer-content">
 
-            <div className="logo">
-                <img src="/assets/images/cabecalho/logo.png" alt="Gente&Gestão Logo" />
-                <span>Gente&Gestão</span>
+            <div className="footer-content">
+
+                <div className="logo">
+                    <img src="/assets/images/cabecalho/logo.png" alt="Gente&Gestão Logo" />
+                    <span>Gente&Gestão</span>
+                </div>
+
+                <div className="contact">
+                    <div className="text">
+                        <h3>Entre em contato</h3>
+                        <p>gente.gestao@gmail.com</p>
+                    </div>
+                    <div className="footer-bottom">
+                        <p>Copyright © 2024 G&G - Todos os direitos reservados à Femme</p>
+                    </div>
+                </div>
+
             </div>
 
-            <div className="contact">
-                <h3>Entre em contato</h3>
-                <p>gente.gestao@gmail.com</p>
-            </div>
-        </div>
 
-        <div className="footer-bottom">
-            <p>Copyright © 2024 G&G - Todos os direitos reservados à Femme</p>
-        </div>
-    </footer>
+        </footer>
 
     )
 
