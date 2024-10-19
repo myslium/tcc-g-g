6210a8055a541f5806c1c2391d45a6bd297749b4
jucastro15 <ra56421610838@acaonsfatima.org.br>
2024-10-19 11:21:57 -0300

    falta a api

--- src/pages/vagas/index.jsx
+++ src/pages/vagas/index.jsx
@@ -1,98 +1,85 @@
-import React from 'react'
-import './index.scss'
+
+import React from 'react';
+import './index.scss';
 import '@fortawesome/fontawesome-free/js/all.js';
-import Tituloelogo from '../../componentes/tituloelogo'
-import Cabecalho from '../../componentes/cabeçalho'
+import Cabecalho from '../../componentes/cabeçalho';
 import Footer from '../../componentes/footer';
 import TituloMenor from '../../componentes/titulomenor';
 
 export default function Vagas() {
     return (
         <div className='pagina-vagas'>
-
             <Cabecalho
-            titulo1 = 'Início'
-            link1='/'
-            titulo2 = 'Sobre G&G'
-            link2 = '/sobre'
-            titulo3 = 'Vagas'
-            link3 = '/vagas'
-            titulo4 = 'Acompanhe o processo'
-            link4 = '/falecomconsultor'
-
+                titulo1='Início'
+                link1='/'
+                titulo2='Sobre G&G'
+                link2='/sobre'
+                titulo3='Vagas'
+                link3='/vagas'
+                titulo4='Acompanhe o processo'
+                link4='/falecomconsultor'
             />
-            
-            <TituloMenor titulo='Vagas'/>
-             
 
-            <div className="botooes">
+            <TituloMenor titulo='Vagas' />
 
-            <div className='bb'>
-                   
-                   <a className='botaobb' href=""> <i className="fa-solid fa-location-dot"></i>&nbsp;Cidade, Estado ou Região</a>
-               </div>
+            <div className="botooes1">
+                <div className="botooes">
+                    <div className="input-container">
+                        <i  className="fa-solid fa-location-dot oi"></i>
+                        <input className='botaobb' type="text" placeholder="Cidade, Estado ou Região" />
+                    </div>
 
-                <div className='bb'>
-                   
-                    <a className='botaobb' href=""> <i className="fa-solid fa-briefcase"></i>&nbsp;Cargos</a>
+                    <div className="input-container">
+                        <i className="fa-solid fa-briefcase oi"></i>
+                        <input className='botaobb' type="text" placeholder="Cargos" />
+                    </div>
+
+                    <div className="input-container">
+                        <i className="fa-solid fa-building oi"></i>
+                        <input className='botaobb' type="text" placeholder="Regime de Trabalho" />
+                    </div>
+
+                    <div className="input-container">
+                        <i className="fa-solid fa-sack-dollar oi"></i>
+                        <input className='botaobb' type="text" placeholder="Remuneração" />
+                    </div>
                 </div>
-                
-                <div className='bb'>
-                   
-                   <a className='botaobb' href=""> <i class="fa-solid fa-building"></i>&nbsp;Regime de Trabalho</a>
-               </div>
 
-                <div className='bb'>
-                   
-                   <a className='botaobb' href=""> <i class="fa-solid fa-sack-dollar"></i>&nbsp;Remuneração</a>
-               </div>
+                <div className="vagas">
+                    <div className="cartavaga">
+                        <div className="textovaga">
+                            <h1 className='profissaovaga'>Analista de Suporte em TI</h1>
+                            <h4>Requisitos</h4>
+                            <ul>
+                                <li>Formação em Administração, Logística ou áreas relacionadas.</li>
+                                <li>Experiência prévia em controle de estoque.</li>
+                                <li>Experiência prévia em controle de estoque.</li>
+                            </ul>
 
-            </div>
-
-            <div className="vagas">
-
-                <div className="cartavaga">
-                    <div className="textovaga">
-                        <h1 className='profissaovaga'>Analista de Suporte em TI</h1>
-                        <h4>Requisitos</h4>
-                        <ul>
-                            <li>Formação em Administração, Logística ou áreas relacionadas.</li>
-                            <li>Experiência prévia em controle de estoque.</li>
-                            <li>Experiência prévia em controle de estoque.</li>
-                        </ul>
-    
-                        <div className='botoazinhos'>
-
-                            <div className='aff'>
-                                <i className="fa-solid fa-location-dot"></i>
-                                &nbsp;São Paulo, SP
+                            <div className='botoazinhos'>
+                                <div className='aff'>
+                                    <i className="fa-solid fa-location-dot"></i>&nbsp;São Paulo, SP
+                                </div>
+                                <div className='aff'>
+                                    <i className="fa-solid fa-briefcase"></i>&nbsp;CLT
+                                </div>
+                                <div className='aff'>
+                                    <i class="fa-solid fa-building"></i>&nbsp;Presencial
+                                </div>
+                                <div className='aff'>
+                                    <i class="fa-solid fa-sack-dollar"></i>&nbsp;R$3.500,00
+                                </div>
                             </div>
-
-                            <div className='aff'>
-                                <i className="fa-solid fa-briefcase"></i>&nbsp;CLT
-                            </div>
-                
-                            <div className='aff'>
-                                <i class="fa-solid fa-building"></i>&nbsp;Presencial
-                            </div>
-                        
-                            <div className='aff'>
-                                <i class="fa-solid fa-sack-dollar"></i>&nbsp;R$3.500,00
-                            </div>
-                        
                         </div>
 
-                    </div>
-
-                    <div className="image">
-                        <a className='botaovagaa' href="">Candidatar-se</a>
+                        <div className="image">
+                            <a className='botaovagaa' href="">Candidatar-se</a>
+                        </div>
                     </div>
                 </div>
-
             </div>
 
-            <Footer/>
-
+            <Footer />
         </div>
-    )
+    );
 }
