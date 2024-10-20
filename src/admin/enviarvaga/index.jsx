5b90fde096f7d5c8fca3d39bd6fde27928f21163
Mariana <165081625+myslium@users.noreply.github.com>
2024-10-20 10:48:33 -0300

    depois vou arrumar a busca por cpf e enviar o email, adicionar funcionou :D

--- src/admin/enviarvaga/index.jsx
+++ src/admin/enviarvaga/index.jsx
@@ -3,18 +3,38 @@ import Cabecalho from '../../componentes/cabeçalho'
 import TituloMenor from '../../componentes/titulomenor'
 import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
-import emailjs from '@emailjs/browser'
+import axios from 'axios'
 
 export default function EnviarVaga() {
 
-    const [empresa, setEmpresa] = useState('')
+    const [descricao, setDescricao] = useState('')
     const [emailEmpresa, setEmailEmpresa] = useState('')
     const [vaga, setVaga] = useState('')
+    const [cpfCandidato, setCpfCandidato] = useState('')
     const [curriculo, setCurriculo] = useState(null)
 
     const navigate = useNavigate()
 
-   
+    async function buscarCPF() {
+        const url = await axios.get(`http://localhost:5010/conferirCandidato/${cpfCandidato}`, {
+            responseType: 'blob'  
+        }) 
+        const resp = window.URL.createObjectURL(new Blob([url.data]))
+        setCurriculo(resp) 
+       
+    }
+
+    async function adicionarCandidatoFinal() {
+
+        const corpo = {
+            "vaga": vaga,
+            "email_empresa": emailEmpresa,
+            "descricao": descricao
+        }
+        const url = `http://localhost:5010/candidatofinal`
+        await axios.post(url, corpo)
+        alert('Candidato final!')
+    }
 
     function reset() {
         localStorage.removeItem('token')
@@ -39,39 +59,34 @@ export default function EnviarVaga() {
 
                 <div className='inputs'>
                         
-                        <label>Empresa:</label>
-                        <input type="text" value={empresa} onChange={e => setEmpresa(e.target.value)}/>
-                        <label>E-mail comercial:</label>
-                        <input type="text" value={emailEmpresa} onChange={e => setEmailEmpresa(e.target.value)}/>
                         <label>Vaga:</label>
                         <input type="text" value={vaga} onChange={e => setVaga(e.target.value)}/>
+                        <label>E-mail comercial:</label>
+                        <input type="text" value={emailEmpresa} onChange={e => setEmailEmpresa(e.target.value)}/>
+                        <label>Descrição:</label>
+                        <input type="text" value={descricao} onChange={e => setDescricao(e.target.value)}/>
                         
                 </div>
 
                         <h1>Selecione os candidatos</h1>
                             <div className='pesquisar'>
-                            <i id='icon'className='fa fa-search'></i>
-                            <input id ='pesquisa-cpf'type="text" placeholder='cpf'/>
+                            <i id='icon' className='fa fa-search' onClick={buscarCPF}></i>
+                            <input id ='pesquisa-cpf'type="text" placeholder='cpf' value={cpfCandidato} onChange={e => setCpfCandidato(e.target.value)}/>
                         </div>
 
                         <div className='selecionados'>
-                        
-                        <div className='candidatos'>
-                               
+                       
+                             <div className='candidatos'>
+                                    <label className='curriculo-label'>
+                                    {curriculo && (
                                   
-                                   <label className='curriculo-label'>
-                                        <input
-                                            type="file"
-                                            onChange={e => setCurriculo(e.target.files[0])}
-                                            style={{ display: 'none' }}
-                                        />
-                                        <span className='curriculo'>Currículo</span>
-                                    </label>
-                               
-                             
-                               
-        
-                        </div>
+                                    <a href={curriculo} download="curriculo.pdf">Baixar Curriculo</a>
+                                )}
+                                            <span className='curriculo'>Currículo</span>
+                                        </label>
+                            </div>
+                       
+                           
                         </div>
                     
                    
@@ -79,7 +94,7 @@ export default function EnviarVaga() {
 
                
             <div className='botao'>
-                    <button>Confirmar</button>
+                    <button onClick={adicionarCandidatoFinal}>Confirmar</button>
             </div>
         </div>
     )
