97b972409314edf4175803266a9e91804effd0b9
fefemariaa <vieiradealmeidam003@gmail.com>
2024-10-19 22:53:39 -0300

    bot pt2

--- src/componentes/intro/index.jsx
+++ src/componentes/intro/index.jsx
@@ -1,21 +1,24 @@
+import './index.scss'
+
 export default function Intro (props){
 
     
     return(   
     <div className="lisa-secao">
-      <div className="lisa">
-
-      </div>
-
+     
+     
       <div className="botons">
-    <button className="botons"><p>Ola sou a Lisa assistente do G&G. Estou aqui para esclarecer suas dúvidas.</p></button>
-    <button className="botons"><p>Veja se algumas dessas corresponde a sua: </p></button>
-    </div>
-
+         <div className="lisa">
+        <img src="/assets/images/consultor/lisinha.png" alt="" />
+        ' </div>
+        <div className="lalo">
+    <button className='butao' ><p>Ola sou a Lisa assistente do G&G. Estou aqui para esclarecer suas dúvidas.</p></button>
+    <button  className='butao'><p>Veja se algumas dessas corresponde a sua: </p></button>
+    </div></div>
     <div className="perguntas">
-    <button onClick={props.cadastro}>Como cadastro minha vaga?</button>
-    <button onClick={props.disponiveis}>Como vejo as vagas disponíveis?</button>
-    <button onClick={props.empresa}>Como cadastro vaga na empresa?</button>
+    <button className='lala' onClick={props.cadastro}><p>Como cadastro minha vaga?</p></button>
+    <button className='lala' onClick={props.disponiveis}><p>Como vejo as vagas disponíveis?</p></button>
+    <button className='lala' onClick={props.empresa}><p>Como cadastro vaga na empresa?</p></button>
     </div>
    
 
