f7171ea181d72058bb1ac18a6afb94074921ba64
Maria Fernanda Vieira de Almeida <ra56756282824@cursos.insf>
2024-10-29 16:40:53 -0300

    responsivo

--- src/admin/notificacoes/index.jsx
+++ src/admin/notificacoes/index.jsx
@@ -6,6 +6,8 @@ import CarrosselVagas from '../../componentes/cardvisao';
 import BarChartComponent from '../../componentes/grafico';
 import axios from 'axios'
 import moment from 'moment';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
 
 import { useNavigate } from 'react-router-dom';
 import './index.scss'
@@ -19,6 +21,40 @@ export default function Notificacoes() {
     const [conteudo, setconteudo] = useState('');
     const [cards, setcards] = useState([]);
     const [alterando, setAlterando] = useState(-1);
+    const [dados, setDados] = useState([]);
+    const [feedback, setFeedback] = useState('');
+  
+    async function fetchData() {
+      try {
+        const cor = ['#c014c0','#681268']
+        const response = await axios.get('http://localhost:5010/formulario/s');
+        const dadosTratados = response.data.map((item, pos) => ({
+          name: `Mês ${item.mes}`,
+          mes: item.quantidade,
+          fill: cor[pos % cor.length]
+        }));
+        setDados(dadosTratados);
+  
+        if (dadosTratados.length >= 2) {
+          const mesPassado = dadosTratados[dadosTratados.length - 2];
+          const mesAtual = dadosTratados[dadosTratados.length - 1];
+  
+          const mesPassadoNome = `Mês ${mesPassado.name.split(' ')[1]}`;
+          const mesAtualNome = `Mês ${mesAtual.name.split(' ')[1]}`;
+          
+          if (mesAtual.mes > mesPassado.mes) {
+            setFeedback(`Seu gráfico mostra que você teve um desempenho melhor em ${mesAtualNome} em comparação a ${mesPassadoNome}. O aumento de ${mesPassado.mes} para ${mesAtual.mes} vagas fechadas é um ótimo sinal. Esse aumento pode ser um reflexo de melhores técnicas de recrutamento ou um aumento na demanda. Continue a analisar o que funcionou bem e considere aplicar essas estratégias em futuros meses.`);
+          } else if (mesAtual.mes < mesPassado.mes) {
+            setFeedback(`Seu gráfico mostra que você teve um desempenho pior em ${mesAtualNome} em comparação a ${mesPassadoNome}. O número de vagas fechadas caiu de ${mesPassado.mes} para ${mesAtual.mes}. É importante revisar as estratégias de recrutamento e identificar possíveis áreas de melhoria para aumentar as vagas fechadas no próximo mês.`);
+          } else {
+            setFeedback(`Seu gráfico mostra que o desempenho se manteve estável entre ${mesAtualNome} e ${mesPassadoNome}. A quantidade de vagas fechadas permaneceu em ${mesAtual.mes}. Considere novas abordagens para impulsionar o fechamento de vagas.`);
+          }
+        }
+      } catch (error) {
+        console.error("Erro ao buscar os dados:", error);
+      }
+    }
+  
 
     async function vernotas() {
         const url = 'http://localhost:5010/inserirNota';
@@ -31,6 +67,7 @@ export default function Notificacoes() {
     useEffect(() => {
     
         vernotas()
+        fetchData()
     }, []);
 
 
@@ -149,7 +186,24 @@ export default function Notificacoes() {
 
             <div className='metas'>
 
-                <BarChartComponent />
+            <div className="barchart-container">
+      <ResponsiveContainer  width="60%" height="100%">
+        <BarChart data={dados}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="mes" fill='#77138b' radius={[5, 5, 0, 0]} />
+        </BarChart>
+      </ResponsiveContainer>
+      {feedback && 
+        <div className="resultado" style={{ marginTop: '10px' }}>
+          <h1>Feedback</h1>
+          <p>{feedback}</p>
+        </div>
+      }
+    </div>
 
             </div>
 
