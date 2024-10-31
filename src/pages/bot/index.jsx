3a27c4099d009581b6ea2b10c29b9828e0de208a
jucastro15 <ra56421610838@acaonsfatima.org.br>
2024-10-30 22:59:06 -0300

    aaaaaaaaaaaaaaaaaaaaaaah socorro deus

--- src/pages/bot/index.jsx
+++ src/pages/bot/index.jsx
@@ -13,11 +13,19 @@ export default function Robo() {
   const [julia, setJulia] = useState([]);
   const [cpf, setCpf] = useState('');
   const [mostrarTabela, setMostrarTabela] = useState(false);
-  const [mostrarInputCpf, setMostrarInputCpf] = useState(false); 
+  const [mostrarInputCpf, setMostrarInputCpf] = useState(false);
   const navigate = useNavigate();
-  let oi = true;
+
+  function resetarEstados() {
+    setResposta('');
+    setRespondendo('');
+    setMostrarTabela(false);
+    setMostrarInputCpf(false);
+    setCpf('');
+  }
 
   function cadastro() {
+    resetarEstados();
     setTimeout(() => {
       setResposta('Para se cadastrar na vaga desejada basta apenas ir no link do Cabeçalho chamado "VAGAS"...');
     }, 1000);
@@ -27,8 +35,7 @@ export default function Robo() {
   }
 
   function disponiveis() {
-    setResposta('');
-    setRespondendo('');
+    resetarEstados();
     setTimeout(() => {
       setResposta('Em nossa plataforma para ter acesso as vagas disponíveis basta ir no link "VAGAS"...');
     }, 1000);
@@ -38,6 +45,7 @@ export default function Robo() {
   }
 
   function empresa() {
+    resetarEstados();
     setTimeout(() => {
       setResposta('Para uma empresa cadastrar uma vaga ou ter interesse em nossos serviços basta ir em "FALE COM O CONSULTOR"...');
     }, 1000);
@@ -47,7 +55,7 @@ export default function Robo() {
   }
 
   function sim() {
-    oi = true;
+    resetarEstados(); 
     setTimeout(() => {
       setRespondendo(
         <Intro
@@ -56,7 +64,7 @@ export default function Robo() {
           disponiveis={disponiveis}
           empresa={empresa}
           cargo={cargo}
-          mostrarCampoCpf={mostrarCampoCpf} 
+          mostrarCampoCpf={mostrarCampoCpf}
         />
       );
     }, 1500);
@@ -66,11 +74,11 @@ export default function Robo() {
     setTimeout(() => {
       alert('Em caso de outra dúvida não esclarecida, fale conosco pelo nosso Email, obrigada!!');
     }, 500);
-    oi = false;
     navigate('/');
   }
 
   function cargo() {
+    resetarEstados();
     setTimeout(() => {
       setResposta('Em nossa página "VAGAS" lá nós temos vagas com acesso filtrado...');
     }, 1000);
@@ -80,7 +88,7 @@ export default function Robo() {
   }
 
   function mostrarCampoCpf() {
-    setMostrarInputCpf(true); 
+    setMostrarInputCpf(true);
   }
 
   async function cpfla() {
@@ -89,99 +97,92 @@ export default function Robo() {
       const resp = await axios.get(url);
       setJulia(resp.data);
       setMostrarTabela(true);
+      setMostrarInputCpf(false); 
+      setTimeout(() => {
+        setRespondendo(<Duvida sim={sim} nao={nao} />);
+      }, 1500);
     } catch (error) {
       console.error("Erro ao buscar CPF:", error);
     }
   }
 
-  if (oi === true) {
-    return (
-      <div className="robo-secao">
-        <Cabecalho
-          titulo1="Início"
-          link1="/"
-          titulo2="Sobre G&G"
-          link2="/sobre"
-          titulo3="Vagas"
-          link3="/vagas"
-          titulo4="Fale com consultor"
-          link4="/falecomconsultor"
-          link5="/bot"
-          titulo5="fa-solid fa-robot"
-          tituloo5="AJUDA"
-          aparecer={true}
+  return (
+    <div className="robo-secao">
+      <Cabecalho
+        titulo1="Início"
+        link1="/"
+        titulo2="Sobre G&G"
+        link2="/sobre"
+        titulo3="Vagas"
+        link3="/vagas"
+        titulo4="Fale com consultor"
+        link4="/falecomconsultor"
+        link5="/bot"
+        titulo5="fa-solid fa-robot"
+        tituloo5="AJUDA"
+        aparecer={true}
+      />
+      <TituloMenor titulo="Lisa assistente" />
+      <div className="cont-robo">
+        <Intro
+          mensagem="Olá, sou a Lisa assistente do G&G. Estou aqui para esclarecer suas dúvidas. As dúvidas abaixo são as mais frequentes pelo nosso público, caso nenhuma delas seja a sua, tudo certo! Fale conosco através do nosso Email e faremos o possível para esclarecer."
+          cadastro={cadastro}
+          disponiveis={disponiveis}
+          empresa={empresa}
+          cargo={cargo}
+          mostrarCampoCpf={mostrarCampoCpf}
         />
-        <TituloMenor titulo="Lisa assistente" />
-
-        <div className="cont-robo">
-          <Intro
-            mensagem="Olá, sou a Lisa assistente do G&G. Estou aqui para esclarecer suas dúvidas. As dúvidas abaixo são as mais frequentes pelo nosso público, caso nenhuma delas seja a sua, tudo certo! Fale conosco através do nosso Email e faremos o possível para esclarecer."
-            cadastro={cadastro}
-            disponiveis={disponiveis}
-            empresa={empresa}
-            cargo={cargo}
-            mostrarCampoCpf={mostrarCampoCpf} 
-          />
-
-          <div className="resposta">
-            <div>
-              <img className="lisaimg" src="/assets/images/consultor/lisinha.png" alt="" />
-            </div>
-
-            <div className="resppergunta">
-              <button className="respbotao">
-                <p className="resp">{resposta ||          
-                mostrarInputCpf && (
-                <div className="cpf-section">
-                  <label>Digite seu CPF:</label>
-                  <input
-                    type="text"
-                    className="cpfinput"
-                    value={cpf}
-                    onChange={(e) => setCpf(e.target.value)}
-                  />
-                  <button onClick={cpfla}>Enviar CPF</button>
-                </div>
-              )}</p>
-                
-      
-              </button>
-              <p className="respondendo">{respondendo}</p>
-
-           
-
-             
-              {mostrarTabela && (
-                <div className="table-container">
-                  <table>
-                    <thead>
-                      <tr>
-                        <th>Nome</th>
-                        <th>Cargo</th>
-                        <th>CPF</th>
-                        <th>Status</th>
+        <div className="resposta">
+          <div>
+            <img className="lisaimg" src="/assets/images/consultor/lisinha.png" alt="" />
+          </div>
+          <div className="resppergunta">
+            <button className="respbotao">
+              <p className="resp">
+                {resposta || (mostrarInputCpf && (
+                  <div className="cpf-section">
+                    <label>Digite seu CPF:</label>
+                    <input
+                      type="text"
+                      className="cpfinput"
+                      value={cpf}
+                      onChange={(e) => setCpf(e.target.value)}
+                    />
+                    <button onClick={cpfla}>Enviar CPF</button>
+                  </div>
+                ))}
+              </p>
+            </button>
+          
+            {mostrarTabela && (
+              <div className="table-container">
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Nome</th>
+                      <th>Cargo</th>
+                      <th>CPF</th>
+                      <th>Status</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {julia.map((item) => (
+                      <tr key={item.id}>
+                        <td>{item.nome}</td>
+                        <td>{item.cargo}</td>
+                        <td>{item.cpf}</td>
+                        <td>{item.status}</td>
                       </tr>
-                    </thead>
-                    <tbody>
-                      {julia.map((item) => (
-                        <tr key={item.id}>
-                          <td>{item.nome}</td>
-                          <td>{item.cargo}</td>
-                          <td>{item.cpf}</td>
-                          <td>{item.status}</td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
-              )}
-
-            </div>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            )}
+              <p className="respondendo">{respondendo}</p>
           </div>
         </div>
       </div>
-    );
-  }
-
-  return null;
+    </div>
+  );
 }
+
