58842e9b92b8ff70f9a32336a1fc4fff70a7f96e
jucastro15 <ra56421610838@acaonsfatima.org.br>
2024-11-02 00:11:05 -0300

    foi

--- src/pages/noterro/index.jsx
+++ src/pages/noterro/index.jsx
@@ -1,16 +1,19 @@
+import { Link } from 'react-router-dom';
 import './index.scss';
 
 
 export default function NotFound() {
 
     return (
+        <Link  to='/'>
         <div className='page-not-found'>
    
-            <div>
+            <div className='not'>
                 <h1 className='titulo'>Error 404</h1>
             <img className='icone-not-found' src='/assets/images/errorimg.jpg' alt=''/>
             </div>
 
         </div>
+        </Link>
     )
 }
