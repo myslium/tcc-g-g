a5e77d98f0cb1955dca0a2b389ca624d4913f09d
jucastro15 <ra56421610838@acaonsfatima.org.br>
2024-11-02 23:10:54 -0300

    foi

--- src/utils/protectedRoute.js
+++ src/utils/protectedRoute.js
@@ -1,12 +1,22 @@
 import { Navigate } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
 
 export default function ProtectedRoute({ children }) {
-    const token = localStorage.getItem('token'); 
-   
-    if (!token) {
+    const token = localStorage.getItem('token');
+
+    if (!token || !isTokenValid(token)) {
         return <Navigate to="/login" />;
     }
-   
 
     return children;
+}
+
+function isTokenValid(token) {
+    try {
+        const decoded = jwt_decode(token);
+        const currentTime = Date.now() / 1000;
+        return decoded.exp > currentTime;
+    } catch (e) {
+        return false;
+    }
 }
