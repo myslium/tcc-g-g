94eaa5d1f6f2921d7b7a2ca78efdcc433e7b8450
jucastro15 <ra56421610838@acaonsfatima.org.br>
2024-11-02 23:31:49 -0300

    nao consegui

--- src/utils/protectedRoute.js
+++ src/utils/protectedRoute.js
@@ -1,25 +1,12 @@
 import { Navigate } from 'react-router-dom';
-import { jwtDecode } from 'jwt-decode'; // Correção aqui
 
 export default function ProtectedRoute({ children }) {
-    const token = localStorage.getItem('token');
-
-    console.log('Token:', token);
-
-    if (!token || !isTokenValid(token)) {
+    const token = localStorage.getItem('token'); 
+   
+    if (!token) {
         return <Navigate to="/login" />;
     }
+   
 
     return children;
-}
-
-function isTokenValid(token) {
-    try {
-        const decoded = jwtDecode(token);
-        const currentTime = Date.now() / 1000;
-        return decoded.exp > currentTime;
-    } catch (e) {
-        console.error('Erro ao decodificar token:', e);
-        return false;
-    }
 }
